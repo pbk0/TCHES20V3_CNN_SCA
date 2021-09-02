ad55becb21559d929fe9ece8a812baaff679cc59
SpikingNeuron <praveenneuron@gmail.com>
2021-09-03 01:01:43 +0200

    add script for download and extract datasets

--- datasets/make.sh
+++ datasets/make.sh
@@ -1,5 +1,7 @@
 wget https://static.data.gouv.fr/resources/ascad-atmega-8515-variable-key/20190903-083349/ascad-variable.h5
 unzip AES_HD_dataset.zip
-unzip AES_RD_dataset.zip
 unzip ASCAD_dataset.zip
 unzip DPAv4_dataset.zip
+
+cat AES_RD_dataset.z* > AES_RD_dataset_full.zip
+unzip AES_RD_dataset_full.zip
